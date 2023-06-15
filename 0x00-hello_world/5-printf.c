6b88294f50b9c4dc2aa2f7abe5550a462bb6f365
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-15 08:11:22 -0700

    Using printf to print output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - prints using printf function with proper grammar, but the outcome is a piece of art,
+ * Return: Always 0
  */
 int main(void)
 {
