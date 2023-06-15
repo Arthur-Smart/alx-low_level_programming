25d33c8a5c78ea4c5bc57d40f844a5dc609cad1e
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-15 07:16:37 -0700

    new commit

--- 0x00-hello_world/program.c
+++ 0x00-hello_world/program.c
@@ -1,6 +1,1 @@
-#include <stdio.h>
-
-int main() {
-    printf("%s, but the outcome is a piece of art.\n", "with proper grammar");
-    return 0;
-}
+#include <stdio.h> int main(void) { printf("Size of char: %lu byte(s)\nSize of int: %lu byte(s)\nSize of float: %lu byte(s)\nSize of double: %lu byte(s)\nSize of long: %lu byte(s)\nSize of long long: %lu byte(s)\nSize of pointer: %lu byte(s)\n", sizeof(char), sizeof(int), sizeof(float), sizeof(double), sizeof(long), sizeof(long long), sizeof(void *)); return 0; }
