133b5626cebae7e5cc0dff95d1efd62f146d2d60
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-15 08:14:19 -0700

    Using printf function in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - prints using printf function with proper grammar, but the outcome is a piece of art,
+ * main - prints using printf function
  * Return: Always 0
  */
 int main(void)
