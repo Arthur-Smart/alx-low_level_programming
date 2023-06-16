3ee1e95a00732c011c9651c7a8bed7d388ed5b37
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-16 02:19:23 -0700

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* more headers goes there */
 #include <stdio.h>
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
 
-/* betty style doc for function main goes there */
+
 int main(void)
 {
 	int n, last_digit;
