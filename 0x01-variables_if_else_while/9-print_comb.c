2a06f39e876dca689e349bfc1c2754495f35c10c
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-16 06:52:22 -0700

    new commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,11 @@
 int main(void)
 {
 	int sd_number;
+
 	for (sd_number = 0; sd_number < 10; sd_number++)
 	{
 	putchar(sd_number + '0');
-	if(sd_number !=9)
+	if (sd_number != 9)
 	{
 	putchar(',');
 	putchar(' ');
