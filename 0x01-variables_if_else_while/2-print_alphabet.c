feb6234c9e5ec6912ed59c917a5264d85f41b91a
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-16 03:26:42 -0700

    new commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,9 +11,9 @@ int main(void)
 {
         char alphabet_in_lowercase;
 
-        for(alphabet_in_lowercase = 'a'; alphabet_in_lowercase <= 'z'; alphabet_in_lowercase++)
+        for (alphabet_in_lowercase = 'a'; alphabet_in_lowercase <= 'z'; alphabet_in_lowercase++)
 	{
-		putchar(alphabet_in_lowercase);
+	putchar(alphabet_in_lowercase);
 	}
 	putchar('\n');
         return (0);
