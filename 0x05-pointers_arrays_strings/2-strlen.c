e0eddc40ced9ef90cc5e92b3914fa52a98193642
Arthur-Smart <mwangiarthur777@gmail.com>
2023-06-26 01:24:46 -0700

    update file

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *__strlen - A function that
+ *_strlen - A function that
  * returns the length of a string
  * @s: The string being checked
  * Return: The lenght of str
  */
-
 int _strlen(char *s)
 {
 	int length = 0;
