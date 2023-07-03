ff6b24310175c7b1e0310fd53fde5ce5bb25d063
Arthur-Smart <mwangiarthur777@gmail.com>
2023-07-03 06:08:17 -0700

    new commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,10 +3,11 @@
 
 /**
  * print_diagsums - Entry point
- * @a: First input
- * @size: Second input
+ * @a: input
+ * @size: input
  * Return: Always 0 (Success)
  */
+
 void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0, b;
