0de5d1f554498d7e2176a66dfbcb02a97bb50e96
Arthur-Smart <mwangiarthur777@gmail.com>
2023-07-03 04:24:21 -0700

    new commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,7 @@
 /**
  * _memset - A function that fills
  * memory with a constant byte
+ * @s: address
  * @n: Bytes taken in account
  * @b: Constant byte
  * Return: Always 0 (Success)
@@ -14,6 +15,5 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (a = 0; a < n; a++)
 	s[a] = b;
-	n--;
 	return (s);
 }
