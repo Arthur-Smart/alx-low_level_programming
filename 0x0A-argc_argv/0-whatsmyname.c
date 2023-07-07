18077b51aff4c7376c5fb0d3c0a3e3bfc355a4af
Arthur-Smart <mwangiarthur777@gmail.com>
2023-07-07 02:21:10 -0700

    0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The c argument check
+ * @argv: The v argument check
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+        (void) argc;
+        printf("%s\n", argv[0]);
+
+        return (0);
+}
