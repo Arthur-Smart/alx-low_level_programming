1c9f9d1dfeeb984f96b87b8363e53392545fe13d
Arthur-Smart <mwangiarthur777@gmail.com>
2023-07-20 01:07:20 -0700

    a function that prints anything.

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - A function that prints anything
+ * @format: Different types of argument
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+}
