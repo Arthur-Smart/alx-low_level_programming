88312bf9b729a46ae9284b146fcaef46e574e72a
Arthur-Smart <mwangiarthur777@gmail.com>
2023-08-01 06:47:57 -0700

    update file

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,9 +8,12 @@
 
 void free_listint(listint_t *head)
 {
+	listint_t *current;
 
-	if (head)
-	{
-	free(head);
-	}
+        while (head)
+        {
+        current = head->next;
+        free(head);
+        head = current;
+        }
 }
